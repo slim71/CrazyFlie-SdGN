0757a976e33707bef78d822199dd1e52e43b8c8b
Simone Vollaro <39267642+slim71@users.noreply.github.com>
2021-03-20 23:02:11 +0100

    Git reformat

---
+++ own_module/log_estimation_managerW.py
@@ -0,0 +1,70 @@
+import logging
+import time
+import cflib.crtp
+from cflib.crazyflie.log import LogConfig
+
+
+class LogEstimationManager:
+
+    # We create a configuration and add all the variables to log and their logging period
+    def __init__(self, cf):
+        self._logconf = LogConfig(name='Estimation', period_in_ms=100)
+        self._cf = cf
+        self._position_estimate = [0, 0, 0]
+        self._attitude_estimate = [0, 0, 0]
+
+    def __enter__(self):
+        cflib.crtp.init_drivers(enable_debug_driver=False)
+        self.config_logging()
+        self.simple_log_async()
+        self.reset_estimator()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print("disconnecting")
+        self._logconf.stop()
+
+    def config_logging(self):
+        self._logconf.add_variable('stateEstimate.x', 'float')
+        self._logconf.add_variable('stateEstimate.y', 'float')
+        self._logconf.add_variable('stateEstimate.z', 'float')
+        self._logconf.add_variable('stabilizer.roll', 'float')
+        self._logconf.add_variable('stabilizer.pitch', 'float')
+        self._logconf.add_variable('stabilizer.yaw', 'float')
+        logging.info("variables added")
+
+    # Callback function to print the contents of logs
+    def callback(self, timestamp, data):
+        print('[%d][%s]: %s' % (timestamp, self._logconf.name, data))
+        self._position_estimate[0] = data['stateEstimate.x']
+        self._position_estimate[1] = data['stateEstimate.y']
+        self._position_estimate[2] = data['stateEstimate.z']
+        self._attitude_estimate[0] = data['stabilizer.roll']
+        self._attitude_estimate[1] = data['stabilizer.pitch']
+        self._attitude_estimate[2] = data['stabilizer.yaw']
+
+    # We add to the Drone framework the previously defined logging configuration
+    def simple_log_async(self):
+        self._cf.log.add_config(self._logconf)    # check if variables are allowed (i.e. are in TOC)
+        if self._logconf.valid:
+            self._logconf.data_received_cb.add_callback(self.callback)
+            self._logconf.start()
+        else:
+            logging.error("Could not add logconfig since some variables are not in TOC")
+
+    def reset_estimator(self):
+        self._cf.param.set_value('stabilizer.estimator', 2)
+        self._cf.param.set_value('kalman.resetEstimation', '1')
+        time.sleep(0.1)
+        self._cf.param.set_value('kalman.resetEstimation', '0')
+
+    def get_estimation(self):
+        return self._position_estimate, self._attitude_estimate
+
+
+'''
+Limitations to be taken into account:
+1)  Each packet is limited to 32bytes, which means that both the data that is logged 
+    and the packet that is sent cannot be larger than this. 
+2)  The minimum period for a log configuration is multiples of 10ms.
+'''
---
+++ own_module/vicon_managerW.py
@@ -0,0 +1,87 @@
+import argparse
+import logging
+
+import numpy as np
+from vicon_dssdk import ViconDataStream
+
+VICON_IP = "192.168.0.2"  # Set the IP of the Vicon server to connect to
+VICON_PORT = "801"  # Set the port of the Vicon server to connect to
+Drone = "Crazyflie"  # Drone "Vicon name"
+Wand = "Active Wand v2 (Origin Tracking)"  # Wand "Vicon-name"
+
+FRAME_NUM = 10
+
+# Extracted from PyVicon Library: (?)
+parser = argparse.ArgumentParser(description=__doc__)
+parser.add_argument("--hostname", "--" + VICON_IP + ":" + VICON_PORT)
+args = parser.parse_args()
+
+
+class ViconManager:
+
+    # Create Client and connect to Server
+    def __init__(self):
+        self._client = ViconDataStream.Client()
+        try:
+            self._client.Connect(VICON_IP + ":" + VICON_PORT)
+        except ViconDataStream.DataStreamException as exc:
+            exit("Can't connect to Vicon! Error: ", exc)
+            # logging.error("Can't connect to Vicon! --> %s", exc)
+
+        self._client.SetBufferSize(FRAME_NUM)  # 1000
+        logging.info("Buffer of %d frames created.", FRAME_NUM)
+
+        self.enable_data()
+
+        got_frame = 0
+        while not got_frame:  # Continue until the stream mode is set and working
+            try:
+                self._client.SetStreamMode(ViconDataStream.Client.StreamMode.EServerPush)
+                got_frame = self._client.GetFrame()
+                logging.debug("Fetched! Pulled frame number: %d" if got_frame
+                              else "Vicon is not streaming!", self._client.GetFrameNumber())
+            except ViconDataStream.DataStreamException as exc:
+                exit("Error using ServerPush mode. --> %s", exc)
+
+        logging.info("Getting available framerates...")  # Show rates of client and server
+        for frameRateName, frameRateValue in self._client.GetFrameRates().items():
+            logging.info("%s : %d Hz \n", frameRateName, frameRateValue)
+
+        self.set_reference_system()
+
+    def get_drone_pos(self):    # TODO check vicon vs crazy frame
+        drone_trans = self._client.GetSegmentGlobalTranslation(Drone, 'Crazyflie')
+        drone_trans_mm = drone_trans[0]
+        drone_trans_m = np.array([float(drone_trans_mm[0]) / 1000,
+                                 -float(drone_trans_mm[1]) / 1000,
+                                  float(drone_trans_mm[2]) / 1000])
+        return drone_trans_m
+
+    def enable_data(self):  # Enable all data types
+        try:
+            self._client.EnableSegmentData()
+            self._client.EnableMarkerData()
+            self._client.EnableUnlabeledMarkerData()
+            self._client.EnableMarkerRayData()
+            self._client.EnableDeviceData()
+            self._client.EnableCentroidData()
+        except ViconDataStream.DataStreamException as exc:
+            logging.error("Couldn't setup data types. --> %s", exc)
+        logging.debug('Segments', self._client.IsSegmentDataEnabled())
+        logging.debug('Markers', self._client.IsMarkerDataEnabled())
+        logging.debug('Unlabeled Markers', self._client.IsUnlabeledMarkerDataEnabled())
+        logging.debug('Marker Rays', self._client.IsMarkerRayDataEnabled())
+        logging.debug('Devices', self._client.IsDeviceDataEnabled())
+        logging.debug('Centroids', self._client.IsCentroidDataEnabled())
+
+    def set_reference_system(self):
+        try:
+            self._client.SetAxisMapping(ViconDataStream.Client.AxisMapping.EForward,
+                                        ViconDataStream.Client.AxisMapping.ELeft,
+                                        ViconDataStream.Client.AxisMapping.EUp)
+        except ViconDataStream.DataStreamException as exc:
+            logging.error("Error while setting axis. --> %s", exc)
+        xAxis, yAxis, zAxis = self._client.GetAxisMapping()
+        logging.info('X Axis: %s', str(xAxis))
+        logging.info('Y Axis: %s', str(yAxis))
+        logging.info('Z Axis: %s', str(zAxis))
