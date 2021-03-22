0328dca4755d5ed8995a4198864e98113a4b6eb8
Simone Vollaro <39267642+slim71@users.noreply.github.com>
2021-03-22 22:59:16 +0100

    Rototranslation experiment

--- test_SV/rototrasl.py
+++ test_SV/rototrasl.py
@@ -1,14 +1,16 @@
+import time
 from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
 from cflib.positioning.motion_commander import MotionCommander
 import script_variables as sc_v
 import script_setup as sc_s
 import logging
 import numpy as np
+from vicon_dssdk import ViconDataStream
 
 sequence = [
-    (0.5, 0, sc_v.DEFAULT_HEIGHT, 0),
-    (0, 0, sc_v.DEFAULT_HEIGHT, 0),
-    (-0.5, 0, sc_v.DEFAULT_HEIGHT, 0)
+    (0.5, 0, sc_v.DEFAULT_HEIGHT),
+    (0, 0, sc_v.DEFAULT_HEIGHT),
+    (-0.5, 0, sc_v.DEFAULT_HEIGHT)
 ]
 
 
@@ -18,16 +20,40 @@ with SyncCrazyflie(sc_v.uri, sc_s.cf) as scf:  # automatic connection
     with MotionCommander(scf, sc_v.DEFAULT_HEIGHT) as mc:
         logging.info('===============Take-Off!================')
 
+        try:
+            sc_s.vicon.GetFrame()
+        except ViconDataStream.DataStreamException as exc:
+            logging.error("Error while getting a frame in the core! "
+                          "--> %s", exc)
+
         sc_v.drone_pos = sc_s.vicon.\
             GetSegmentGlobalTranslation(sc_v.drone, sc_v.drone)[0]
-        sc_v.drone_pos = np.array(sc_v.drone_pos, dnmin=2)
+        position = np.array(sc_v.drone_pos, dnmin=2)
+
+        orientation = sc_s.vicon.\
+            GetSegmentGlobalRotationEulerXYZ(sc_v.drone, sc_v.drone)[0]
 
         sc_v.drone_or = sc_s.vicon.\
             GetSegmentGlobalRotationMatrix(sc_v.drone, sc_v.drone)[0]
-        sc_v.drone_or = np.mat(sc_v.drone_or)
+        rot_matrix = np.mat(sc_v.drone_or)
 
         rototrasl = np.concatenate(
-            (np.concatenate((sc_v.drone_or, sc_v.drone_or.T), axis=1),
+            (np.concatenate((rot_matrix, position.T), axis=1),
              np.array([[0, 0, 0, 1]])), axis=0)
 
-        point = np.array(sequence[0], ndmin=2)
+        for point in sequence:
+            for i in range(10):
+                reference = np.concatenate(
+                    (np.array(point, ndmin=2).T, np.array([1], ndmin=2)),
+                    axis=0)
+
+                setpoint = np.matmul(rototrasl, reference)
+
+                scf.cf.extpos.send_extpos(position[0],
+                                          position[1],
+                                          position[2])
+                scf.cf.commander.send_position_setpoint(setpoint[0, 3],
+                                                        setpoint[1, 3],
+                                                        setpoint[2, 3],
+                                                        -orientation[2])
+                time.sleep(0.1)
