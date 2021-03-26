2924393249f5346af1115d817571184864c4076b
Simone Vollaro <39267642+slim71@users.noreply.github.com>
2021-03-27 00:31:14 +0100

    Deleted useless code

--- own_module/crazyfun.py
+++ own_module/crazyfun.py
@@ -1,10 +1,5 @@
 # Module containing all the function used in the Crazyflie project
 
-# TODO: complete functions help
-
-# -----------------------------------------------------------------------------
-# ----------------------------------IMPORT-------------------------------------
-# -----------------------------------------------------------------------------
 from __future__ import print_function
 import logging
 import os
@@ -17,10 +12,6 @@ import math
 import time
 import __main__
 
-# Max number of consecutive loss allowed during acquisition of the setpoint
-# of the Wand or drone
-MAX_LOSS = 10
-
 log_pos_x = 0
 log_pos_y = 0
 log_pos_z = 0
@@ -28,47 +19,7 @@ log_roll = 0
 log_pitch = 0
 log_yaw = 0
 
-# Security offset
-SEC_OFFSET = 0.3  # [m]
-
-# The height the drone has to reach at the end of the take-off. This can't
-# be higher than the "MC_HEIGHT" used in the class of the
-# Motion Commander. We suggest to set it at least at 90% of its value.
-DEFAULT_HEIGHT = 0.5  # [m]
-
-# This is the default height used by the Motion Commander during take-off.
-# It has to be higher than DEFAULT_HEIGHT because we consider the
-# take-off phase concluded once the drone reaches DEFAULT_HEIGHT, but this
-# can't be always true because the Vicon might observe another value due to
-# noise
-MC_HEIGHT = 0.8  # [m]
-
-# Number of frames for the Vicon buffer
-frame_num = 1000
-
-# -----------------------------------------------------------------------------
-# ----------------------------------FUNCTIONS----------------------------------
-# -----------------------------------------------------------------------------
-
-
-# This function resets the Kalman Filter.
-# TODO: already in the Crazyflie object!
-def reset_estimator(crazyflie):
-    """
-    Resets the estimator.
-
-        :param crazyflie: Object linked to the Crazyflie
-        :return: None
-
-    """
-
-    # The reset command is active on rising edge, so setting from 0 to 1 resets
-    # the KF. We set it back to 0 so that next time the script is run it is
-    # already reset.
-    # https://forum.bitcraze.io/viewtopic.php?t=2746
-    crazyflie.param.set_value('kalman.resetEstimation', '1')
-    time.sleep(0.1)
-    crazyflie.param.set_value('kalman.resetEstimation', '0')
+# TODO: complete functions help
 
 
 def log_stab_callback(timestamp, data, log_conf):
@@ -140,7 +91,7 @@ def matlab_print(*args):
     # can use a \n as argument to get a newline
     # Create a string with all data passed to the function
     s = ""
-    for arg in args:
+    for arg in args[:-1]:
         s = s + "{} ".format(str(arg))
 
     file_name = os.path.normpath(__main__.__file__).split(os.sep)[-1][:-3]
@@ -159,9 +110,7 @@ def print_callback(data):
     """
     Prints gathered data to a specific file.
 
-        :param timestamp: Timestamp for the log file
         :param data: Data to be logged
-        :param log_conf: Representation of a log configuration
         :return:None
 
     """
@@ -222,192 +171,6 @@ def datalog(sync_crazyflie):
     data_log_async(sync_crazyflie, measure_log)
 
     return measure_log
-
-
-def getFirstPosition(vicon_client, drone_obj):
-    """
-    Gathers drone setpoint information from the Vicon client.
-
-    :param vicon_client: Vicon client to connect to
-    :param drone_obj: Drone considered
-    :return drone_pos_m: Drone setpoint in the Vicon system, in meters
-    """
-
-    drone_pos_m = np.array([0.0, 0.0, 0.0])
-    iterations = 0
-
-    # It can happen that the setpoint value [0,0,0] is received
-    # more than once. This means that something wrong is
-    # happening with the Vicon Tracking.
-    # So, in order to avoid errors, we count how many times we receive [0,0,0]
-    # and if it exceeds MAX_LOSS we stop the experiment.
-    while (drone_pos_m[0] == 0) \
-            and (drone_pos_m[1] == 0) \
-            and (drone_pos_m[2] == 0):
-        # This call is necessary before each call to some
-        # "GetSegment...()"-like function
-        a = vicon_client.GetFrame()
-        b = vicon_client.GetFrameNumber()
-
-        drone_tuple = vicon_client. \
-            GetSegmentGlobalTranslation(drone_obj, drone_obj)
-
-        # We are interested only in the first part of this structure
-        drone_pos_mm = drone_tuple[0]
-
-        # Absolute setpoint of the drone converted from [mm] to [m]
-        drone_pos_m = np.array([float(drone_pos_mm[0]) / 1000,
-                                float(drone_pos_mm[1]) / 1000,
-                                float(drone_pos_mm[2]) / 1000]
-                               )
-
-        if iterations > MAX_LOSS:
-            print("WARNING: initial setpoint of the drone reported ",
-                  "[0,0,0] for ",
-                  MAX_LOSS,
-                  "consecutive times. Please try with another initial "
-                  "setpoint and restart the experiment."
-                  )
-            exit()
-        else:
-            iterations += 1
-
-    return drone_pos_m
-
-
-def createMatrixRotation(vicon_client, drone_obj, drone_trans_m, last_gamma,
-                         kf_quat):
-    """
-    Creates the homogeneous matrix used for the conversion to the
-    Navigation system (corresponding to the drone initial setpoint before
-    take-off) from the Global system (Vicon reference system).
-    The matrix is comprised of:
-    - Rotation:   we assume always NULL values for the roll and pitch angles,
-                  so the rotation is on the Z-axis with yaw angle "gamma",
-                  expressed with a 3x3 matrix
-    - Translation: it's the initial setpoint of the drone. It is rotated with
-                   the previous matrix.
-
-    :param vicon_client: Vicon client
-    :param drone_obj: Vicon name of the drone
-    :param drone_trans_m: Translation of the drone in the Vicon reference
-            system
-    :param last_gamma: Last recorded value for the yaw angle
-    :param kf_quat: Flag to include quaternions into KF
-
-    :return:
-        HomMatrix: Homogeneous transformation matrix to drone body,
-        last_gamma: Last valid yaw angle value,
-        RotMatrix: Rotation matrix to drone body
-    """
-
-    if kf_quat:
-        # In this case we don't need to take in consideration the
-        # orientation of the drone in the conversion between frames:f
-        # this will be the Kalman filter's concern
-        gamma = 0
-        last_gamma = 0
-
-    else:
-        # Otherwise the K-F doesn't receive updates of the yaw angle from
-        # us, so we take it in consideration in the Matrix
-        # in order to correctly convert between frames.
-
-        # We get the orientation of the drone from which we want to get the
-        # Yaw angle
-        euler = vicon_client. \
-            GetSegmentGlobalRotationEulerXYZ(drone_obj, drone_obj)
-        # We are interested only in the first part of the structure:
-        euler_radians = euler[0]
-        logging.debug("Euler angles: %s", str(euler_radians))
-
-        # In case we receive tha value [0,0,0], that means an error with the
-        # Vicon Tracker, we continue using the previous value of the yaw angle
-        if (euler_radians[0] == 0
-                and euler_radians[1] == 0
-                and euler_radians[2] == 0):
-            gamma = last_gamma
-        else:
-            gamma = euler_radians[2]
-            last_gamma = gamma
-
-    # Roll angle:
-    alpha = 0
-    # Pitch angle:
-    beta = 0
-
-    # Build the homogeneous rotation matrix that will convert a
-    # setpoint expressed in the Vicon reference system
-    # in setpoint expressed in the Body frame of the drone
-    R_x = np.array([[1, 0, 0],
-                    [0, math.cos(alpha), -math.sin(alpha)],
-                    [0, math.sin(alpha), math.cos(alpha)]])
-    R_y = np.array([[math.cos(beta), 0, math.sin(beta)],
-                    [0, 1, 0],
-                    [-math.sin(beta), 0, math.cos(beta)]])
-    R_z = np.array([[math.cos(gamma), -math.sin(gamma), 0],
-                    [math.sin(gamma), math.cos(gamma), 0],
-                    [0, 0, 1]])
-    Rot_xy = np.dot(R_x, R_y)  # R_x * R_y
-    Rot_xyz = np.dot(Rot_xy, R_z)  # R_xy * R_z
-    RotMatrix = np.transpose(Rot_xyz)  # R_xyz'
-    HomMatrix = np.array([[RotMatrix[0, 0],
-                           RotMatrix[0, 1],
-                           RotMatrix[0, 2],
-                           -np.dot(RotMatrix,
-                                   np.transpose(drone_trans_m))[0]
-                           ],
-                          [RotMatrix[1, 0],
-                           RotMatrix[1, 1],
-                           RotMatrix[1, 2], -
-                           np.dot(RotMatrix,
-                                  np.transpose(drone_trans_m))[1]
-                           ],
-                          [RotMatrix[2, 0],
-                           RotMatrix[2, 1],
-                           RotMatrix[2, 2], -
-                           np.dot(RotMatrix,
-                                  np.transpose(drone_trans_m))[2]
-                           ],
-                          [0, 0, 0, 1]])
-
-    # In case we update the last value of the yaw angle (not null value) we
-    # update returning it together with the matrices
-    return HomMatrix, RotMatrix, last_gamma
-
-
-def landing(last_pos, sub_height, de_height, crazyflie, lg_stab,
-            fdesc, log_flag):
-    """
-    This function implements the landing phase.
-
-    :param last_pos:
-    :param sub_height: Total meters subtracted to the drone height
-    :param de_height: Amount to subtract each time from the drone height
-    :param crazyflie: Crazyflie object linked to the drone
-    :param lg_stab: Log stabilizer
-    :param fdesc: Descriptor of the log file
-    :param log_flag: Flag specifying if log info should be produced
-    :return: None
-
-    """
-
-    # Gradually reduce the height setpoint
-    while last_pos[2] - sub_height > 0:
-        crazyflie.commander.send_position_setpoint(last_pos[0],
-                                                   last_pos[1],
-                                                   last_pos[2] - sub_height,
-                                                   0)
-        sub_height = sub_height + de_height
-
-    if log_flag:
-        # Close the connection with the log table
-        lg_stab.stop()
-        fdesc.close()
-
-    exit()
-
-    return
 
 
 def wait_for_position_estimator(scf):
