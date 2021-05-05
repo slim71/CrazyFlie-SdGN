6061f17474840a6d3ddf4a21ee7e9d6ef1184475
Simone Vollaro <39267642+slim71@users.noreply.github.com>
2021-05-06 00:03:15 +0200

    Trying high-level commander

--- test_SV/high_level.py
+++ test_SV/high_level.py
@@ -11,6 +11,7 @@ import numpy as np
 
 with SyncCrazyflie(sc_v.uri, sc_s.cf) as scf:
     scf.cf.param.set_value('stabilizer.estimator', 2)  # set KF as estimator
+    scf.cf.param.set_value('commander.enHighLevel', '1')
     crazy.reset_estimator(scf)
     # crazy.wait_for_position_estimator(scf)
 
