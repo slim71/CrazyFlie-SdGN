d980c35f2d8d608fb5f2b9e84795459f3491c007
Simone Vollaro <39267642+slim71@users.noreply.github.com>
2021-05-14 12:52:24 +0200

    Cleaned setup files

--- own_module/script_setup.py
+++ own_module/script_setup.py
@@ -1,8 +1,6 @@
 import os
-import __main__
 import logging
 from datetime import datetime
-import numpy as np
 from cflib import crazyflie
 import script_variables as sc_v
 from cflib import crtp
@@ -16,15 +14,13 @@ filename = "../experiment_logs/" + file_name + \
 logname = filename + ".log"
 
 # Only logs of level ERROR or above will be tracked
-# https://docs.python.org/3/library/logging.html#levels
+# ref: https://docs.python.org/3/library/logging.html#levels
 logging.basicConfig(filename=logname,
                     level=logging.DEBUG,
                     filemode='a',
                     format="%(asctime)s [%(levelname)s]: %(message)s")
 
 # -------------------------------CONNECTION-----------------------------------
-
-# Extracted from PyVicon Library
 logging.info("Connecting to the Vicon...")
 
 # Create a VICON Client
@@ -35,16 +31,18 @@ vicon = ViconDataStream.Client()
 try:
     vicon.Connect(sc_v.VICON_IP + ":" + sc_v.VICON_PORT)
 except ViconDataStream.DataStreamException as exc:
-    logging.error("Can't connect to Vicon! --> %s", exc)
+    logging.error("Can't connect to Vicon! --> %s", str(exc))
     exit("Can't connect to Vicon! --> " + str(exc))
+
 logging.info("Connected to Vicon!")
 
+# -----------------------------VICON SETTINGS---------------------------------
 # Setting a buffer size to work with
 # logging.info("Setting up the buffer...")
 # vicon.SetBufferSize(sc_v.buffer_size)
 # logging.info("Buffer of %d frames created.", sc_v.buffer_size)
 
-# Enable all the data types (action needed to be able to use these)
+# Enable all the data types (action needed to use them)
 logging.info("Enabling data types...")
 try:
     vicon.EnableSegmentData()
@@ -54,61 +52,73 @@ try:
     vicon.EnableDeviceData()
     vicon.EnableCentroidData()
 except ViconDataStream.DataStreamException as exc:
-    logging.error("Couldn't setup data types. --> %s", exc)
+    logging.error("Couldn't setup data types --> %s", str(exc))
+    # Report data types toggle status
+    logging.debug('Segments: %s', str(vicon.IsSegmentDataEnabled()))
+    logging.debug('Markers: %s', str(vicon.IsMarkerDataEnabled()))
+    logging.debug('Unlabeled Markers: %s',
+                  str(vicon.IsUnlabeledMarkerDataEnabled()))
+    logging.debug('Marker Rays: %s', str(vicon.IsMarkerRayDataEnabled()))
+    logging.debug('Devices: %s', str(vicon.IsDeviceDataEnabled()))
+    logging.debug('Centroids: %s ', str(vicon.IsCentroidDataEnabled()))
+    exit("Couldn't setup data types --> " + str(exc))
 
-# Report whether the data types have been enabled
-logging.debug('Segments: %s', str(vicon.IsSegmentDataEnabled()))
-logging.debug('Markers: %s', str(vicon.IsMarkerDataEnabled()))
-logging.debug('Unlabeled Markers: %s',
-              str(vicon.IsUnlabeledMarkerDataEnabled()))
-logging.debug('Marker Rays: %s', str(vicon.IsMarkerRayDataEnabled()))
-logging.debug('Devices: %s', str(vicon.IsDeviceDataEnabled()))
-logging.debug('Centroids: %s ', str(vicon.IsCentroidDataEnabled()))
+logging.info("Data types enabled")
 
-#           Trying to set different stream modes
+# Attempts to set different stream modes
 logging.info("Testing stream modes...")
-# "ClientPull": Increases latency, network bandwidth kept at minimum,
-# buffers unlikely to be filled up
+# 1. "ClientPull": increases latency; network bandwidth kept at minimum;
+#                  buffers unlikely to be filled up
 logging.info("Getting a frame in ClientPull mode...")
 try:
     vicon.SetStreamMode(ViconDataStream.Client.StreamMode.EClientPull)
     logging.debug("Fetched! Pulled frame number: %d" if vicon.GetFrame()
-                  else "Vicon is not streaming!", vicon.GetFrameNumber())
+                  else "Vicon is not streaming in ClientPull mode!",
+                  vicon.GetFrameNumber())
 except ViconDataStream.DataStreamException as exc:
-    logging.error("Error using ClientPull mode. --> %s", exc)
-# "ClientPreFetch": improved ClientPull, server performances unlikely to be
-# affected, latency slightly reduced, buffers unlikely to be filled up
-logging.info("Getting a frame in PreFetch mode...")
+    logging.error("Error using ClientPull mode --> %s", str(exc))
+logging.info("ClientPull mode available.")
+
+# 2. "ClientPreFetch": improved ClientPull; server performances unlikely to be
+#                      affected; latency slightly reduced; buffers unlikely to 
+#                      be filled up
+logging.info("Getting a frame in ClientPreFetch mode...")
 try:
     vicon.SetStreamMode(ViconDataStream.Client.StreamMode.EClientPullPreFetch)
     logging.debug("Fetched! Pulled frame number: %s" if vicon.GetFrame()
-                  else "Vicon is not streaming!", str(vicon.GetFrameNumber()))
+                  else "Vicon is not streaming in ClientPreFetch mode!",
+                  str(vicon.GetFrameNumber()))
 except ViconDataStream.DataStreamException as exc:
-    logging.error("Error using ClientPreFetch mode. --> %s", exc)
-# "ServerPush": the servers pushes frames to the client, best for latency,
-# frames dropped only if all buffers are full
+    logging.error("Error using ClientPreFetch mode --> %s", str(exc))
+logging.info("ClientPreFetch mode available.")
+
+# 3. "ServerPush": the servers pushes frames to the client; best for latency,
+#                  frames dropped only if all buffers are full
+#
+# Continue until ServerPush mode is set and working, since it's the one we
+# intend to use
 logging.info("Getting a frame in ServerPush mode...")
-# Continue until this mode is set and working, since it's the one we intend
-# to use
 while not sc_v.got_frame:
     try:
         vicon.SetStreamMode(ViconDataStream.Client.StreamMode.EServerPush)
         sc_v.got_frame = vicon.GetFrame()
         logging.debug("Fetched! Pulled frame number: %d" if sc_v.got_frame
-                      else "Vicon is not streaming!", vicon.GetFrameNumber())
+                      else "Vicon is not streaming in ServerPush mode!",
+                      vicon.GetFrameNumber())
     except ViconDataStream.DataStreamException as exc:
-        logging.error("Error using ServerPush mode. --> %s", exc)
+        logging.error("Error using ServerPush mode --> %s", str(exc))
 logging.info("ServerPush mode set.")
+sc_v.got_frame = 0
 
-# Show the frame rate from both client and server side
+# Show the framerate from both client and server side
 logging.info("Getting available framerates...")
 try:
-    logging.info('Current Vicon frame rate: %s Hz.', vicon.GetFrameRate())
-    logging.info('Available frame rates: ')
+    logging.info('Current Vicon framerate: %s Hz.', vicon.GetFrameRate())
+    logging.info('Available framerates:')
     for frameRateName, frameRateValue in vicon.GetFrameRates().items():
-        logging.info("%s : %d Hz \n", frameRateName, frameRateValue)
+        logging.info("%s : %d Hz \n", str(frameRateName), frameRateValue)
 except ViconDataStream.DataStreamException as exc:
-    logging.error("Framerate error. --> %s", exc)
+    logging.error("Framerate error. --> %s", str(exc))
 
 # Setting reference system (Vicon standard: X forward, Y left, Z up)
 logging.info("Setting axis...")
@@ -117,72 +127,32 @@ try:
                          ViconDataStream.Client.AxisMapping.ELeft,
                          ViconDataStream.Client.AxisMapping.EUp)
 except ViconDataStream.DataStreamException as exc:
-    logging.error("Error while setting axis. --> %s", exc)
+    logging.error("Error while setting axis. --> %s", str(exc))
 # Check and report Axis
 xAxis, yAxis, zAxis = vicon.GetAxisMapping()
-logging.info('X Axis: %s', str(xAxis))
-logging.info('Y Axis: %s', str(yAxis))
-logging.info('Z Axis: %s', str(zAxis))
+logging.info('X axis: %s', str(xAxis))
+logging.info('Y axis: %s', str(yAxis))
+logging.info('Z axis: %s', str(zAxis))
 
 # Initialize all the drivers
 logging.info("Initializing drivers...")
 crtp.init_drivers(enable_debug_driver=False)
 logging.info("Drivers initialized.")
 
-# DRONE CONNECTION
+# ----------------------------DRONE CONNECTION--------------------------------
 logging.info('Connecting to the Crazyflie...')
 
 # Creating an instance of the Crazyflie object
 cf = crazyflie.Crazyflie()
 
-# Test to check Segment and Subject names
+# -----------------------------VICON CHECKS-----------------------------------
+
+# Test to check Segment and Subject names in Vicon
 subs = vicon.GetSubjectNames()
 for each_sub in subs:
     segs = vicon.GetSegmentNames(each_sub)
-    logging.info("%s has the following segments: %s", each_sub, segs)
+    logging.info("%s has the following segments: %s", str(each_sub), str(segs))
     root = vicon.GetSubjectRootSegmentName(each_sub)
     logging.info("%s has root: %s", str(each_sub), str(root))
 
-logging.debug("===Checking functions without flying===")
-attempts = 0
-got_frame = 0  # reset flag
-# while attempts < 10:
-#     try:
-#         got_frame = vicon.GetFrame()
-#         logging.debug("Fetched! Pulled frame number: %d" if got_frame
-#                       else "Vicon is not streaming!", vicon.GetFrameNumber())
-#         if not got_frame:
-#             continue
-#     except ViconDataStream.DataStreamException as exc:
-#         exit("Error while getting frame in check mode: " + str(exc))
-#         logging.error("Error while getting frame in check mode! --> %s", exc)
-#
-#     # all in Global Vicon -> RPY in global
-#     try:
-#         mat_gl = vicon.GetSegmentGlobalRotationMatrix(sc_v.drone, sc_v.drone)
-#         # logging.debug("Drone rotation matrix from Vicon: %s %s %s ",
-#         #               str(mat_gl[0]), str(mat_gl[1]), str(mat_gl[2]))
-#         logging.debug("Drone rotation matrix from Vicon: %s",
-#                       str(mat_gl))
-#     except ViconDataStream.DataStreamException as exc:
-#         exit("Error while managing the rotation: " + str(exc))
-#         logging.error("Error while managing the rotation! --> %s", exc)
-#
-#     try:
-#         eul = vicon.GetSegmentGlobalRotationEulerXYZ(sc_v.drone, sc_v.drone)
-#         eul = np.array(eul[0])
-#         logging.debug("Drone Euler XYZ from Vicon as given: %s", str(eul))
-#     except ViconDataStream.DataStreamException as exc:
-#         exit("Error while managing Euler angles: " + str(exc))
-#         logging.error("Error while managing Euler angles! --> %s", exc)
-#
-#     try:
-#         drone_trans = vicon.GetSegmentGlobalTranslation(sc_v.drone, sc_v.drone)
-#         logging.debug("Drone position from global origin: %s",
-#                       str(drone_trans[0]))
-#     except ViconDataStream.DataStreamException as exc:
-#         exit("Error while getting drone translation: " + str(exc))
-#         logging.error("Error while getting drone translation! --> %s", exc)
-#     attempts += 1
-
-logging.debug("===============Offline check done=================")
+logging.debug("===============SETUP DONE=================")
--- own_module/script_variables.py
+++ own_module/script_variables.py
@@ -1,66 +1,30 @@
 import numpy as np
 
+# Vicon connection settings
 VICON_IP = "192.168.0.2"  # Set the IP of the Vicon server to connect to
 VICON_PORT = "801"  # Set the port of the Vicon server to connect to
 drone = "Crazyflie2"  # Set the "Vicon name" of the object linked to the drone
 
-# Set the "Vicon-name" of the object relative to the Wand
+# Set the "Vicon-name" of the Wand
 Wand = "Active Wand v2 (Origin Tracking)"
-uri = 'radio://0/80/2M'  # Used for the connection with the drone
 
-WAIT_TIME = 5  # [s]  Used for the management of the iterations
-SLEEP_TIME = 0.001  # [s]  Used for the management of the iterations
-ITERATIONS = WAIT_TIME / SLEEP_TIME  # Used to manage the iterations
+# Crazyflie address
+uri = 'radio://0/80/2M'
 
-# Security offset
-SEC_OFFSET = 0.3  # [m]
-
-# To be subtracted from the "z" component of the last position of the drone
-# during each iteration of the landing phase
-DELTA_HEIGHT = 0.01  # [m]
-
-# Sum of meters subtracted from the  "z" component of
-# the last position of the drone
-SUBTRACTED_HEIGHT = 0.01  # [m]
-
-# The height the drone has to reach at the end of the take-off. This can't
-# be higher than the "MC_HEIGHT" used in the class of the
-# Motion Commander. We suggest to set it at least at 90% of its value.
+# The height the drone has to reach at the end of the take-off.
 DEFAULT_HEIGHT = 0.5  # [m]
 
-# This is the default height used by the Motion Commander during take-off.
-# It has to be higher than DEFAULT_HEIGHT because we consider the
-# take-off phase concluded once the drone reaches DEFAULT_HEIGHT, but this
-# can't be always true because the Vicon might observe another value due to
-# noise
-MC_HEIGHT = 0.8  # [m]
-
-# Number of frames for the Vicon buffer
-buffer_size = 1000
-
+# Utility flags
 got_frame = 0
 last_frame = 0
 new_frame = 0
-
-# Current number of consecutive losses in the acquisition of the Wand position
-CONSEC_LOSSES = 0
-
-# Used to update the yaw angle of the drone during the experiment
-last_gamma = 0
+vicon_conn = 0
+attempts = 10
 
 # Used in the generation of the position reference for the drone
-# TODO: to delete?
-last_trans = np.array([0.0, 0.0, 0.0])
-last_drone_pos = np.array([0.0, 0.0, 0.0])
-last_drone_ref = np.array([0.0, 0.0, 0.0])
-drone_pos_m = np.array([0.0, 0.0, 0.0])
-drone_or = np.mat([[0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0]])
+drone_or = np.mat([0.0, 0.0, 0.0, 0.0])
 drone_pos = np.array([0.0, 0.0, 0.0])
 last_wand_pos = np.array([0.0, 0.0, 0.0])
 wand_pos_m = np.array([0.0, 0.0, 0.0])
 wand_pos = np.array([0.0, 0.0, 0.0])
 wand_trans = np.array([0.0, 0.0, 0.0])
-
-is_deck_attached = False
-# TODO: to delete?
-position_estimate = [0, 0]
