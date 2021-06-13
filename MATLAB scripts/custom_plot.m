5925bc678e9676d26dda3dad6d45c06513639c10
Simone Vollaro <39267642+slim71@users.noreply.github.com>
2021-06-14 00:17:37 +0200

    Revert "Improved MATLAB script"
    This reverts commit dedc8593b762da04d4b61371024ca9ab8f920c86.

--- MATLAB scripts/custom_plot.m
+++ MATLAB scripts/custom_plot.m
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1b310f15a398f2cfd75d70f31785893d7b5fd783335c85b47c933cca0947aaa2
-size 7775
+oid sha256:b61db7f3377a6b5fa05a4b142b6a812d4f410a537848e9087b39a287008511bc
+size 7360
