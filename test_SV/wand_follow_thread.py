65c243606c4b99f3749edd6a08f7cd7e2c4076a6
Simone Vollaro <39267642+slim71@users.noreply.github.com>
2021-06-13 22:19:15 +0200

    Made threads as daemons

--- test_SV/wand_follow_thread.py
+++ test_SV/wand_follow_thread.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:87c2b18b24af3c68570ebbfd33896f15b6b90e7c2c5352bf83b981d12f8bae83
-size 1959
+oid sha256:0ed3180da9911528759b3c64b6af66aa69a25b1c7e9934f1817a20cf5e82153c
+size 2299
